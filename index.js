02e33e8b2092e4fbfbedae688d89c7f02e4e2388
Cory Owen <cjowen14@outlook.com>
2022-01-05 14:26:00 -0700

    Completed Core Lab

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("Hello World");
 
-
+let message = document.querySelector("#message");
 
 function addMovie(event){
     event.preventDefault();
@@ -17,15 +17,22 @@ function addMovie(event){
     let newUl = document.querySelector("ul");
     newUl.appendChild(movie);
     inputField.value = '';
+    message.textContent = 'Movie Added!'
 }
 
 function deleteMovie(event){
     event.target.parentNode.remove();
+    message.textContent = 'Movie Deleted!';
 }
 
 function crossOffMovie(event){
     console.log(event);
     event.target.classList.toggle("checked");
+    if(event.target.classList.contains("checked")){
+        message.textContent = "Movie Watched!"
+    }else{
+        message.textContent = "Movie AddedBack!"
+    }
 }
 
 let newForm = document.querySelector("form");
