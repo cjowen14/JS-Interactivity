1e0a462e33f25a43cb908da5ca2201ce1502df42
Cory Owen <cjowen14@outlook.com>
2022-01-05 14:08:55 -0700

    Completed Step 2 and 3

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 console.log("Hello World");
+
+
+
+function addMovie(event){
+    event.preventDefault();
+    let inputField = document.querySelector("input");
+    let movie = document.createElement("li");
+    let movieTitle = document.createElement("span");
+    movieTitle.textContent = inputField.value;
+    movie.appendChild(movieTitle);
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = 'X';
+    deleteBtn.addEventListener("click", deleteMovie);
+    movie.appendChild(deleteBtn);
+    let newUl = document.querySelector("ul");
+    newUl.appendChild(movie);
+    inputField.value = '';
+}
+
+function deleteMovie(event){
+    event.target.parentNode.remove();
+}
+
+let newForm = document.querySelector("form");
+newForm.addEventListener("submit", addMovie);
+
